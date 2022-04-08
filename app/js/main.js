ff76d8a449f7ad077376dda567c615adbff2ed83
kosm-dev <mnyh2019@gmail.com>
2022-04-08 15:18:29 +0300

    added section hits and benefits

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,11 @@
-$(function() {
+$(function () {
+
+  $(".star").rateYo({
+    starWidth: "15px",
+    normalFill: "transparent",
+    ratedFill: "#E2510E",
+    readOnly: true,
+    spacing: "5px",
+    starSvg: '<svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.5 1.19376L9.162 4.79696L9.27922 5.05111L9.55716 5.08406L13.4976 5.55126L10.5843 8.24537L10.3788 8.43539L10.4334 8.70991L11.2067 12.6019L7.74422 10.6637L7.5 10.527L7.25578 10.6637L3.79328 12.6019L4.56661 8.70991L4.62115 8.43539L4.41567 8.24537L1.5024 5.55126L5.44284 5.08406L5.72078 5.05111L5.838 4.79696L7.5 1.19376Z" stroke="#E2510E"/>'
+  });
 });
