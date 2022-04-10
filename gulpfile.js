b9b5893844165a244e9780a5a955fbcedd168008
kosm-dev <mnyh2019@gmail.com>
2022-04-10 10:57:12 +0300

    added section price

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/choices.js/public/assets/scripts/choices.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
